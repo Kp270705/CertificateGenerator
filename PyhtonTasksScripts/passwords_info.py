8e4b90fc110f5e43c640a095a3e13660da3ff403
Kunal Pathak <112516865+Kp270705@users.noreply.github.com>
2024-09-23 01:18:09 +0530

    Delete PyhtonTasksScripts directory

--- PyhtonTasksScripts/passwords_info.py
+++
@@ -1,7 +0,0 @@
-
-# passwords:
-
-# mail id's and their passwords:
-mail1_pass = f"ohdg qwud lhfd bxwi" # by kpkpkunal2005@gmail.com for kp145419@gmail.com
-mail1b_pass = f"saer lhke fzwd hizc" # by kpkpkunal2005@gmail.com for kunalpathak4774@gmail.com
-mail2_pass = f"bolp xadc fhdo nmok" # by kunalpathak4774@gmail.com for kpkpkunal2005@gmail.com
