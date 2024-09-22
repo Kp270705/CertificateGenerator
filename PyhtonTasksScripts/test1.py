aba65aed75ca13d4c8559cf90587ea6e2fd52070
Kp270705 <kunalpathak4774@gmail.com>
2024-09-23 00:51:56 +0530

    adding python scripts directory

---
+++ PyhtonTasksScripts/test1.py
@@ -0,0 +1,16 @@
+def upper_case(word):
+    word = word.upper()
+    return word
+
+normalStr = "hello sir how r u"
+capStr = upper_case(normalStr)
+print(f"capitalize string: {capStr}")
+
+
+# ---------------------------------------------------------------
+
+# from csvFunc import processFile
+
+# from PyhtonTasksScripts.csvFunc import processFile
+# from PyhtonTasksScripts.generate_pdf import getData
+# from PyhtonTasksScripts.genZip import zip_folder
